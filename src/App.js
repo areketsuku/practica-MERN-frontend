ce6876058babc162433021062dc8b96c42131593
areketsuku <alexcaldasaixarch@gmail.com>
2025-12-07 10:37:19 +0100

    feature: basic API + basic App ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from "react";
+import {createContact, getAllContacts, /*getContact, updateContact,*/ deleteContact} from "./api/contacts.js";
 
-function App() {
+function App (){
+  const [contacts, setContacts] = useState([]);
+  const [form, setForm] = useState({name: "",phone1: ""});
+
+  //Carreguem info
+  useEffect(()=>{
+    loadContacts();
+  },[]);
+
+  async function loadContacts() {
+    const data = await getAllContacts();
+    setContacts(data);
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await createContact(form);
+    setForm({name:"",phone1:""});
+    loadContacts();
+  }
+
+  async function handledelete(id) {
+    await deleteContact(id);
+    loadContacts();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Llibreta de Contactes</h1>
+      
+      <h2>Gestiónar Contactes</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Nom"
+          value={form.name}
+          onChange={e =>setForm({...form, name: e.target.value})}
+        />
+        <br/>
+        <input
+          placeholder="Telefon 1"
+          value={form.phone1}
+          onChange={e =>setForm({...form, phone1: e.target.value})}
+        />
+        <br />
+        <button type="submit">Crear</button>
+      </form>
+
+      <h2>Llista de Contactes</h2>
+      <ul>
+        {contacts.map(contact =>(
+          <li key={contact.id}>
+            **{contact.name}** — {contact.email} — {contact.phone1}
+            <button onClick={()=>handledelete(contact.id)}>Esborrar</button>
+          </li>
+        ))};
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/api/contacts.js
@@ -0,0 +1,46 @@
+const API_URL = process.env.REACT_APP_API_URL+"/api/contacts";
+
+//Create
+export async function createContact(data){
+    const res = await fetch (API_URL,{
+        method: "POST",
+        headers:{"Content-Type":"application/json"},
+        body: JSON.stringify(data)
+    });
+    if (!res.ok) throw new Error("Error while creating contact");
+    return res.json();
+};
+
+//Read all
+export async function getAllContacts(){
+    const res = await fetch (API_URL);
+    if (!res.ok) throw new Error("Error while geting all contacts");
+    return res.json();
+};
+
+//Read one
+export async function getContact(id){
+    const res = await fetch (`${API_URL}/${id}`);
+    if (!res.ok) throw new Error("Error while geting contact");
+    return res.json();
+};
+
+//Update
+export async function updateContact(id, data) {
+    const res = await fetch (`${API_URL}/${id}`,{
+        method: "PUT",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(data)
+    });
+    if (!res.ok) throw new Error("Error while updating contact");
+    return res.json();  
+};
+
+//Delete
+export async function deleteContact(id) {
+    const res = await fetch (`${API_URL}/${id}`,{
+        method: "DELETE"
+    });
+    if (!res.ok) throw new Error("Error while deleting contact");
+    return res.json();0  
+};
