f879075d16ad2e1da79a0819b6365c78b36f6f42
areketsuku <alexcaldasaixarch@gmail.com>
2025-12-11 12:03:55 +0100

    feature: new inputs & empty button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {createContact, getAllContacts, deleteContact, updateContact} from "./api
 function App (){
   const [allContacts, setAllContacts] = useState([]);
   const [contacts, setContacts] = useState([]);
-  const [form, setForm] = useState({name: "",email:"",phone1: ""});
+  const [form, setForm] = useState({name:"",surname1:"",surname2:"",email:"",phone1:"",phone2:""});
   const [editingId, setEditingId] = useState(null);
 
   useEffect(()=>{
@@ -19,7 +19,7 @@ function App (){
 
   function filterContacts(formData) {
 
-    const isEmpty = !formData.name && !formData.email && !formData.phone1;
+    const isEmpty = !formData.name && !formData.surname1 && !formData.surname2 && !formData.email && !formData.phone1 && !formData.phone2 ;
     
     if (isEmpty) {
       setContacts(allContacts);
@@ -28,17 +28,30 @@ function App (){
 
     const filtered = allContacts.filter(contact => {
       const matchesName = contact.name?.toLowerCase().includes(formData.name.toLowerCase());
+      const matchesSurname1 = contact.surname1?.toLowerCase().includes(formData.surname1.toLowerCase());
+      const matchesSurname2 = contact.surname2?.toLowerCase().includes(formData.surname2.toLowerCase());
       const matchesEmail = contact.email?.toLowerCase().includes(formData.email.toLowerCase());
       const matchesPhone1 = contact.phone1?.toString().includes(formData.phone1.toString());
+      const matchesPhone2 = contact.phone2?.toString().includes(formData.phone2.toString());
 
       return (
         (formData.name ? matchesName : true) &&
+        (formData.surname1 ? matchesSurname1 : true) &&
+        (formData.surname2 ? matchesSurname2 : true) &&
         (formData.email ? matchesEmail : true) &&
-        (formData.phone1 ? matchesPhone1 : true)
+        (formData.phone1 ? matchesPhone1 : true) &&
+        (formData.phone2 ? matchesPhone2 : true)
       );
     });
     setContacts(filtered);
   }
+
+  function handleReset (e) {
+    e.preventDefault();
+    setContacts(allContacts);
+    setForm({name:"",surname1:"",surname2:"",email:"",phone1:"",phone2:""});
+    setEditingId(null);
+  };
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -48,7 +61,7 @@ function App (){
     } else {
       await createContact(form);
     }
-    setForm({name:"",email:"",phone1:""});
+    setForm({name:"",surname1:"",surname2:"",email:"",phone1:"",phone2:""});
     loadContacts();
   }
 
@@ -61,8 +74,11 @@ function App (){
     setEditingId(contact._id);
     setForm({
       name: contact.name,
+      surname1: contact.surname1,
+      surname2: contact.surname2,
       email: contact.email,
-      phone1: contact.phone1
+      phone1: contact.phone1,
+      phone2: contact.phone2
     });
   }
 
@@ -71,12 +87,32 @@ function App (){
       <h1>Llibreta de Contactes</h1>
       
       <h2>Gestiónar Contactes</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <input
           placeholder="Nom"
           value={form.name}
           onChange={e => {
             const updated = {...form, name: e.target.value};
+            setForm(updated);
+            filterContacts(updated);
+            }}
+        />
+        <br/>
+        <input
+          placeholder="Primer Cognom"
+          value={form.surname1}
+          onChange={e => {
+            const updated = {...form, surname1: e.target.value};
+            setForm(updated);
+            filterContacts(updated);
+            }}
+        />
+        <br/>
+                <input
+          placeholder="Segon Cognom"
+          value={form.surname2}
+          onChange={e => {
+            const updated = {...form, surname2: e.target.value};
             setForm(updated);
             filterContacts(updated);
             }}
@@ -102,14 +138,26 @@ function App (){
             }}
         />
         <br />
+        <input
+          placeholder="Telefon 2"
+          value={form.phone2}
+          onChange={e => {
+            const updated = {...form, phone2: e.target.value};
+            setForm(updated);
+            filterContacts(updated);
+            }}
+        />
+        <br />
         <button type="submit">{editingId?"Guardar":"Crear"}</button>
+        <button type="reset">Buida</button>
+
       </form>
 
       <h2>Llista de Contactes</h2>
       <ul>
         {contacts.map(contact =>(
           <li key={contact._id}>
-            <b>{contact.name}</b> - {contact.email} - {contact.phone1} - <em />
+            <b>{contact.name} {contact.surname1} {contact.surname2}</b> - {contact.email} - {contact.phone1} / {contact.phone2} <em />
             <button onClick={()=>handleDelete(contact._id)}>Esborrar</button>
             <button onClick={()=>handleEdit(contact)}>Editar</button>
           </li>
